884b5b15a6996b96369500561acb6dc1a1dbaba4
Fiodar Luhauskoi <lugovsckoi@gmail.com>
2019-03-22 15:36:28 +0300

    add: add index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,25 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;  
+  var lv_triangles_col = 0;
+  var len = preferences.length;
+
+  for(let i = 0; i < len; i++) { 
+
+    var f_lvr = preferences[i];
+    var s_lvr = preferences[f_lvr - 1];
+    var t_lvr = preferences[s_lvr - 1];
+
+    if(t_lvr == i + 1) {
+      lv_triangles_col++;
+    }
+
+    if (lv_triangles_col == 3) {
+      count++;
+      lv_triangles_col = 0;
+    }
+  }
+
+  return(count);
 };
